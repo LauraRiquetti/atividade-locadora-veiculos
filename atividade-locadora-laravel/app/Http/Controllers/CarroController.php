b085f81c3539050a3d8a897982fb4dd0f0af3311
LauraRiquetti <laurariquetti@gmail.com>
2026-02-04 21:38:06 -0300

    new - base do projeto
    Base do projeto

---
+++ atividade-locadora-laravel/app/Http/Controllers/CarroController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Carro;
+use Illuminate\Http\Request;
+
+class CarroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $carros = Carro::orderByDesc('id')->get();
+
+        return view('carros.index', compact('carros'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('carros.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'modelo' => 'required|string',
+            'placa'=> 'required|string|unique:carros,placa,' . $id,
+            'marca' => 'required|string',
+            'ano' => 'required|integer',
+            'preco_diaria' => 'required|decimal',
+            'descricao' => 'text',
+        ]);
+        $aluno->update([
+            'modelo' =>$request->modelo,
+            'placa' =>$request->placa,
+            'marca' =>$request->marca,
+            'ano' =>$request->ano,
+            'preco_diaria' =>$request->preco_diaria,
+            'descricao' =>$request->descricao,
+        ]);
+
+        return redirect()->route('alunos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ atividade-locadora-laravel/database/migrations/2026_02_04_234015_create_carros_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->string('modelo');
+            $table->string('placa')->unique;
+            $table->string('marca');
+            $table->integer('ano');
+            $table->decimal('preco_diaria');
+            $table->text('descricao')->opcional;
+            $table->timestamps();
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('carros');
+    }
+};
